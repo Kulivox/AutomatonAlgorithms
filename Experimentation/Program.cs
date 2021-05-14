9909051927af912a54bfb9a7effb38450f61dbfd
Michal <mich.klinka@gmail.com>
2021-05-14 10:03:39 +0200

    added Experimentation.csproj

---
+++ Experimentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Experimentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
